1cfda3246a970caafd87b0e1bc61a607b66c58cd
Cedar-mkt <sand_vir57ginppp@yahoo.co.jp>
2019-12-15 17:02:23 +0900

    update action of index+create+update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,16 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
   end
 
   def create
-    Group.create(group_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -13,14 +18,20 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
